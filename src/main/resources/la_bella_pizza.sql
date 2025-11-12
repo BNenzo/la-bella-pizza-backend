e8f8505fb7a37dc5cec352123799f4d86403ad2c
BNenzo <enzoletona06@gmail.com>
2025-11-12 12:50:43 -0300

    feat: modify SQL template

--- src/main/resources/la_bella_pizza.sql
+++ src/main/resources/la_bella_pizza.sql
@@ -53,7 +53,7 @@ INSERT INTO provincias (cod_provincia, nom_provincia) VALUES
 
 CREATE TABLE localidades (
     nro_localidad INT PRIMARY KEY,
-    nom_localidad VARCHAR(100) NOT NULL,
+    nom_localidad VARCHAR(50) NOT NULL,
     cod_provincia CHAR(5) NOT NULL,
     FOREIGN KEY (cod_provincia) REFERENCES provincias(cod_provincia)
 );
@@ -69,15 +69,15 @@ CREATE TABLE categorias_precios (
 );
 
 INSERT INTO categorias_precios VALUES
-(1, 'Económico'), (2, 'Moderado'), (3, 'Premium'), (4, 'Lujo');
+(1, 'Económico/Bajo'), (2, 'Medio'), (3, 'Alto/Premium'), (4, 'De lujo');
 
 CREATE TABLE restaurantes (
     nro_restaurante INT PRIMARY KEY,
-    razon_social VARCHAR(150) NOT NULL,
+    razon_social VARCHAR(50) NOT NULL,
     cuit CHAR(11) UNIQUE NOT NULL
 );
 
-INSERT INTO restaurantes VALUES (1, 'La Bella Pizza', '30714567891');
+INSERT INTO restaurantes VALUES (1, 'La Bella Pizza', '30717101975');
 
 CREATE TABLE sucursales (
     nro_restaurante INT NOT NULL,
@@ -99,8 +99,8 @@ CREATE TABLE sucursales (
 );
 
 INSERT INTO sucursales VALUES
-(1,1,'Sucursal Alta Córdoba','Av. Colón',850,'Alta Córdoba',1,'5000','351-4567890',120,10,2),
-(1,2,'Sucursal General Paz','Av. Hipólito Yrigoyen',350,'General Paz',1,'5000','351-4781234',80,15,3);
+(1,1,'La Bella Pizza Alta Córdoba','Juan Antonio Lavalleja',2344,'Alta Córdoba',1,'5001','03512317731',50,15,2),
+(1,2,'La Bella Pizza General Paz','Jacinto Ríos',170,'General Paz',1,'5004','03515388931',30,12,3);
 
 CREATE TABLE zonas (
     cod_zona CHAR(5) PRIMARY KEY,
@@ -113,7 +113,23 @@ INSERT INTO zonas (cod_zona, nom_zona) VALUES
 ('ACBA','Alta Córdoba'),
 ('GPZ','General Paz'),
 ('CDLR','Cerro de las Rosas'),
-('VBEL','Villa Belgrano');
+('VBEL','Villa Belgrano'),
+('JUNI','Jardín'),
+('URCA','Urca'),
+('SANV','San Vicente'),
+('OBLA','Observatorio'),
+('ARGU','Argüello'),
+('MAIP','Maipú'),
+('POET','Poeta Lugones'),
+('RSME','Residencial San Martín'),
+('IPON','Iponá'),
+('GUIZ','Guiñazú'),
+('PATR','Patricios'),
+('VILL','Villa Cabrera'),
+('VILLP','Villa Páez'),
+('SAGU','San Gerónimo'),
+('LOSGL','Los Gigantes'),
+('BAJO','Bajo Alberdi');
 
 
 CREATE TABLE zonas_sucursales (
@@ -129,7 +145,7 @@ CREATE TABLE zonas_sucursales (
     FOREIGN KEY (cod_zona) REFERENCES zonas (cod_zona)
 );
 
-INSERT INTO zonas_sucursales VALUES (1,1,'ACBA',20,1,1),(1,2,'GPZ',20,1,1);
+INSERT INTO zonas_sucursales VALUES (1,1,'ACBA',50,1,1),(1,2,'GPZ',30,1,1);
 
 CREATE TABLE turnos_sucursales (
     nro_restaurante INT NOT NULL,
@@ -143,8 +159,10 @@ CREATE TABLE turnos_sucursales (
 );
 
 INSERT INTO turnos_sucursales VALUES
-(1,1,'12:00','15:30',1),(1,1,'20:00','23:30',1),
-(1,2,'12:00','15:00',1),(1,2,'20:00','23:00',0);
+(1,1,'12:00','13:30',1),(1,1,'20:00','21:30',1),
+(1,1,'13:30','14:30',1),(1,1,'21:30','22:30',1),
+(1,1,'14:30','15:30',1),(1,1,'22:30','23:30',1),
+(1,2,'12:00','13:15',1),(1,2,'20:00','21:00',0);
 
 CREATE TABLE zonas_turnos_sucursales (
     nro_restaurante INT NOT NULL,
@@ -152,7 +170,6 @@ CREATE TABLE zonas_turnos_sucursales (
     hora_desde TIME NOT NULL,
     cod_zona CHAR(5) NOT NULL,
     permite_menores INT NOT NULL DEFAULT 1,
-    -- PK en el MISMO orden que usa la FK
     PRIMARY KEY (nro_restaurante, nro_sucursal, cod_zona, hora_desde),
     FOREIGN KEY (nro_restaurante, nro_sucursal, hora_desde)
         REFERENCES turnos_sucursales (nro_restaurante, nro_sucursal, hora_desde),
@@ -172,7 +189,7 @@ CREATE TABLE clientes (
 );
 
 INSERT INTO clientes VALUES
-(1,'Pérez','Juan','juan.perez@example.com','351-1234567'),
+(1,'Letona','Renzo','renzo.letona@example.com','351-1112233'),
 (2,'Gómez','María','maria.gomez@example.com','351-2345678'),
 (3,'Rodríguez','Lucas','lucas.rodriguez@example.com','351-3456789'),
 (4,'Fernández','Ana','ana.fernandez@example.com','351-4567890'),
@@ -206,8 +223,19 @@ CREATE TABLE estilos (
     nom_estilo VARCHAR(100) NOT NULL
 );
 
+
 INSERT INTO estilos VALUES
-(1,'Parrilla'),(2,'Italiana'),(3,'Vegetariana / Vegana'),(4,'Pizzería');
+(1, 'Gourmet'),
+(2, 'Casual'),
+(3, 'Comida rápida / Fast food'),
+(4, 'Buffet libre'),
+(5, 'Bistró'),
+(6, 'Food truck'),
+(7, 'Restaurante tradicional'),
+(8, 'Bar / Tapas'),
+(9, 'Cafetería'),
+(10, 'Delivery'),
+(11, 'Fine dining');
 
 CREATE TABLE estilos_sucursales (
     nro_restaurante INT NOT NULL,
@@ -221,7 +249,7 @@ CREATE TABLE estilos_sucursales (
 );
 
 INSERT INTO estilos_sucursales VALUES
-(1,1,1,1),(1,1,4,1),(1,2,2,1),(1,2,3,0);
+(1,1,2,1),(1,1,3,1),(1,2,2,1),(1,2,3,0);
 
 CREATE TABLE tipos_comidas (
     nro_tipo_comida INT PRIMARY KEY,
@@ -229,8 +257,21 @@ CREATE TABLE tipos_comidas (
 );
 
 INSERT INTO tipos_comidas VALUES
-(1,'Carnes'),(2,'Pastas'),(3,'Pescados y Mariscos'),(4,'Comida Vegetariana'),
-(5,'Comida Vegana'),(6,'Pizzas'),(7,'Postres'),(8,'Bebidas');
+(1, 'Italiana'),
+(2, 'Mexicana'),
+(3, 'Española'),
+(4, 'Francesa'),
+(5, 'Japonesa'),
+(6, 'China'),
+(7, 'Tailandesa'),
+(8, 'India'),
+(9, 'Mediterránea'),
+(10, 'Argentina'),
+(11, 'Peruana'),
+(12, 'Árabe / Medio Oriente'),
+(13, 'Americana'),
+(14, 'Fusión'),
+(15, 'Internacional');
 
 CREATE TABLE tipos_comidas_sucursales (
     nro_restaurante INT NOT NULL,
@@ -244,7 +285,7 @@ CREATE TABLE tipos_comidas_sucursales (
 );
 
 INSERT INTO tipos_comidas_sucursales VALUES
-(1,1,1,1),(1,1,6,1),(1,1,7,1),(1,2,2,1),(1,2,4,1),(1,2,8,1);
+(1,1,1,1),(1,2,1,1);
 
 CREATE TABLE especialidades_alimentarias (
     nro_restriccion INT PRIMARY KEY,
@@ -252,8 +293,13 @@ CREATE TABLE especialidades_alimentarias (
 );
 
 INSERT INTO especialidades_alimentarias VALUES
-(1,'Sin gluten'),(2,'Sin lactosa'),(3,'Vegetariana'),
-(4,'Vegana'),(5,'Apta para diabéticos'),(6,'Baja en sodio'),(7,'Sin frutos secos');
+(1, 'Vegetariana'),
+(2, 'Vegana'),
+(3, 'Sin gluten / Celíaco'),
+(4, 'Sin lactosa'),
+(5, 'Baja en calorías'),
+(6, 'Orgánica'),
+(7, 'Diabéticos (sin azúcar añadida)');
 
 CREATE TABLE especialidades_alimentarias_sucursales (
     nro_restaurante INT NOT NULL,
@@ -268,7 +314,7 @@ CREATE TABLE especialidades_alimentarias_sucursales (
 );
 
 INSERT INTO especialidades_alimentarias_sucursales VALUES
-(1,1,1,1),(1,1,2,1),(1,1,5,1),(1,2,3,1),(1,2,4,1),(1,2,6,0);
+(1,1,1,1),(1,1,2,1),(1,2,1,1),(1,2,2,1);
 
 CREATE TABLE contenidos (
     nro_restaurante INT NOT NULL,
@@ -285,9 +331,9 @@ CREATE TABLE contenidos (
 );
 
 INSERT INTO contenidos VALUES
-(1,1,'Promo mediodía: menú parrilla + bebida','https://imgs.example.com/promo_mediodia.jpg',1,15.00,1),
-(1,2,'Noche de pizzas a la piedra 2x1','https://imgs.example.com/pizzas_2x1.jpg',1,12.50,1),
-(1,3,'Pastas caseras en sucursal Nueva Cba','https://imgs.example.com/pastas.jpg',0,10.00,2);
+(1,1,'Promo mediodía: Pizza a la piedra + bebida','https://tn.com.ar/resizer/z2Dke2M5Hbz4s3VRE_OClr_-fXU=/arc-anglerfish-arc2-prod-artear/public/FOTWE3GMANB6BPQKQB4GER55MM.jpeg',1,15.00,1),
+(1,2,'Noche de pizzas a la piedra 2x1','https://www.paulinacocina.net/wp-content/uploads/2024/05/receta-de-pizza-frita-paulina-cocina-recetas-800x450.jpg',1,12.50,1),
+(1,3,'Degustacion de pizzas en sucursal Alta Cba','https://external-preview.redd.it/dominos-50-off-pizza-deal-returns-april-21-27-2025-v0-fmRa26hiSj0oi3Ob8jddYxIJCAft4z0H26lGC1J9KvE.jpg?width=640&crop=smart&auto=webp&s=34ace06ed3c90f079c718796a0ce7496ea4f5f32',0,10.00,2);
 
 CREATE TABLE clicks_contenidos (
     nro_restaurante INT NOT NULL,
@@ -301,27 +347,6 @@ CREATE TABLE clicks_contenidos (
         REFERENCES contenidos (nro_restaurante, nro_contenido),
     FOREIGN KEY (nro_cliente) REFERENCES clientes (nro_cliente)
 );
-
-
-
--- SELECT
---     s.nro_restaurante,
---     s.nro_sucursal,
---     s.nom_sucursal,
---     s.calle,
---     s.nro_calle,
---     s.barrio,
---     l.nom_localidad AS localidad,
---     p.nom_provincia AS provincia,
---     s.cod_postal,
---     s.telefonos,
---     s.total_comensales,
---     s.min_tolerancia_reserva,
---     c.nom_categoria AS categoria
--- FROM sucursales s
--- JOIN localidades l ON s.nro_localidad = l.nro_localidad
--- JOIN provincias p ON l.cod_provincia = p.cod_provincia
--- JOIN categorias_precios c ON s.nro_categoria = c.nro_categoria;
 
 IF OBJECT_ID('sp_get_provincias', 'P') IS NOT NULL
     DROP PROCEDURE sp_get_provincias;
@@ -380,7 +405,6 @@ BEGIN
     BEGIN CATCH
         IF XACT_STATE() <> 0 ROLLBACK TRAN;
 
-        -- Reexpone el error original con su severidad/estado.
         THROW;
     END CATCH
 END;
