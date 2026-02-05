0acb4a5377f4b1c136a519ea487140069a1f7afa
BNenzo <enzoletona06@gmail.com>
2026-02-05 17:38:50 -0300

    feat: implementacion de ver reservas

--- src/main/java/ar/edu/ubp/das/la_bella_pizza_backend/repositories/LaBellaPizzaRepository.java
+++ src/main/java/ar/edu/ubp/das/la_bella_pizza_backend/repositories/LaBellaPizzaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+import ar.edu.ubp.das.la_bella_pizza_backend.beans.ActualizarReservaClienteRequestBean;
 import ar.edu.ubp.das.la_bella_pizza_backend.beans.ClicksContenidosRestaurantesBean;
 import ar.edu.ubp.das.la_bella_pizza_backend.beans.ContenidoNoPublicadoBean;
 import ar.edu.ubp.das.la_bella_pizza_backend.components.SimpleJdbcCallFactory;
@@ -39,4 +40,22 @@ public class LaBellaPizzaRepository {
         "contenidos_no_publicados",
         ContenidoNoPublicadoBean.class);
   }
+
+  // ACTUALIZAR LA RESERVA DE UN CLIENTE
+  public void actualizarReservaCliente(ActualizarReservaClienteRequestBean request) {
+    System.out.println(request.getCodReservaSucursal());
+    MapSqlParameterSource params = new MapSqlParameterSource()
+        .addValue("cod_reserva", request.getCodReservaSucursal())
+        .addValue("cant_adultos", request.getCantAdultos())
+        .addValue("fecha_reserva", request.getFechaReserva())
+        .addValue("hora_reserva", request.getHoraReserva())
+        .addValue("fecha_cancelacion", request.getFechaCancelacion())
+        .addValue("cancelada", request.getFechaCancelacion() != null ? 1 : null);
+
+    jdbcCallFactory.executeWithOutputs(
+        "sp_actualizar_reserva_cliente",
+        "dbo",
+        params);
+  }
+
 }
